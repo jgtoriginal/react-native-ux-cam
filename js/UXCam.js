73ff21102b51757ab7f53bcbb99253e8f5814c4c
Joaquin GT <jgtoriginal@gmail.com>
2018-12-20 11:28:09 -0300

    first commit

---
+++ js/UXCam.js
@@ -0,0 +1,71 @@
+// Libraries
+import {NativeModules} from 'react-native';
+
+// Native Modules
+const {RNUXCam} = NativeModules;
+
+
+class UXCam {
+
+  // --------------------------------------------------
+  // Initialize
+  // --------------------------------------------------
+  startWithKey(key) {
+    return RNUXCam.startWithKey(key);
+  }
+
+  stopApplicationAndUploadData() {
+    return RNUXCam.stopApplicationAndUploadData();
+  }
+
+  restartSession() {
+    return RNUXCam.restartSession();
+  }
+
+  setAutomaticScreenNameTagging(enableScreenNameTagging) {
+    return RNUXCam.setAutomaticScreenNameTagging(enableScreenNameTagging);
+  }
+
+  // --------------------------------------------------
+  // Occlude
+  // --------------------------------------------------
+  occludeSensitiveScreen(shouldOcclude) {
+    return RNUXCam.occludeSensitiveScreen(shouldOcclude);
+  }
+
+  // --------------------------------------------------
+  // Tags
+  // --------------------------------------------------
+  tagScreenName(screenName) {
+    return RNUXCam.tagScreenName(screenName);
+  }
+
+  tagUserName(userName) {
+    // Native expects a string so we have to coerce any integers
+    // to strings.
+    return RNUXCam.tagUserName(userName.toString());
+  }
+
+  addTag(tag, properties = {}) {
+    return RNUXCam.addTag(tag, properties);
+  }
+
+  markSessionAsFavorite() {
+    return RNUXCam.markSessionAsFavorite();
+  }
+
+  // --------------------------------------------------
+  // URLs
+  // --------------------------------------------------
+  async urlForCurrentUser() {
+    return RNUXCam.urlForCurrentUser();
+  }
+
+  async urlForCurrentSession() {
+    return RNUXCam.urlForCurrentSession();
+  }
+
+}
+
+
+export default new UXCam();
